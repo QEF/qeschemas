b904a206696b90bd5f36a2fe7075b9cbdae03fc6
Jusong Yu <jusong.yeu@gmail.com>
2022-09-13 15:02:53 +0200

    Update generated page

--- .make_ghpages/generate.sh
+++ .make_ghpages/generate.sh
@@ -6,4 +6,12 @@ xsltproc --stringparam title "UPF v2.0.1" \
          --stringparam printLegend false \
          --stringparam printGlossary false \
          --stringparam printNSPrefixes false \
-         .make_ghpages/xs3p.xsl UPF/qe_upf_220905.xsd > out/qe_upf.html
+         .make_ghpages/xs3p.xsl UPF/qe_pp-v2.xsd > out/qe_pp-v2.html
+
+xsltproc --stringparam title "UPF v3.0.0" \
+         --stringparam sortByComponent false \
+         --stringparam printAllSuperTypes false \
+         --stringparam printLegend false \
+         --stringparam printGlossary false \
+         --stringparam printNSPrefixes false \
+         .make_ghpages/xs3p.xsl UPF/qe_pp-v3.xsd > out/qe_pp-v3.html
